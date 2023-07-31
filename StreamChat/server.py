b24ab252b12e0e9548793f6fec6f8a2c001d96c2
ixs_koki.chin <koki.chin@ixs.co.jp>
2023-07-31 18:38:15 +0900

    add demo chat

--- StreamChat/server.py
+++ StreamChat/server.py
@@ -1,46 +1,98 @@
-import asyncio
-import uvicorn
+import threading
+import queue
 
-from typing import AsyncIterable, Awaitable
-from dotenv import load_dotenv
+import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import FileResponse, StreamingResponse
-from langchain.callbacks import AsyncIteratorCallbackHandler
+
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import HumanMessage
+from langchain.callbacks.manager  import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-load_dotenv()
-async def wait_done(fn: Awaitable, event: asyncio.Event):
+from langchain.schema import (
+    HumanMessage,
+    SystemMessage,
+    AIMessage
+)
+
+from langchain.memory import ConversationBufferMemory
+
+app = FastAPI(
+    title="Demo chat",
+)
+
+memory = [SystemMessage(content="You are a helpful assistant.")]
+
+class ThreadedGenerator:
+    def __init__(self):
+        self.queue = queue.Queue()
+        self.msg = []
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        item = self.queue.get()
+        if item is StopIteration:
+            raise item
+        return item
+
+    def send(self, data):
+        self.queue.put(data)
+        self.msg += [data]
+
+    def close(self):
+        self.queue.put(StopIteration)
+
+
+class ChainStreamHandler(StreamingStdOutCallbackHandler):
+    def __init__(self, gen):
+        super().__init__()
+        self.gen = gen
+
+    def on_llm_new_token(self, token: str, **kwargs):
+        self.gen.send(token)
+
+
+def llm_thread(g, prompt):
+    global memory
     try:
-        await fn
-    except Exception as e:
-        print(e)
-        event.set()
+        chat = ChatOpenAI(verbose=True,streaming=True,
+            callback_manager=CallbackManager([ChainStreamHandler(g)]),
+            # temperature=0.7,
+        )
+        memory.append(HumanMessage(content=prompt))
+        chat(memory)
+        # chat([SystemMessage(content="You are a helpful assistant."),HumanMessage(content=prompt)])
+
     finally:
-        event.set()
-
-async def call_openai(question: str) -> AsyncIterable[str]:
-    callback = AsyncIteratorCallbackHandler()
-    model = ChatOpenAI(streaming=True, verbose=True, callbacks=[callback])
-
-    coroutine = wait_done(model.agenerate(messages=[[HumanMessage(content=question)]]), callback.done)
-    task = asyncio.create_task(coroutine)
-
-    async for token in callback.aiter():
-        yield f"{token}"
-
-    await task
+        memory.append(AIMessage(content=''.join(g.msg)))
+        g.close()
 
 
-app = FastAPI()
+def chat(prompt):
+    g = ThreadedGenerator()
+    threading.Thread(target=llm_thread, args=(g, prompt)).start()
+    return g
+
+
+@app.get("/test-stream")
+async def stream():
+    return StreamingResponse(
+        chat("Do you know vuejs?"),
+        media_type='text/event-stream'
+    )
 
 @app.post("/ask")
 def ask(body: dict):
-    return StreamingResponse(call_openai(body['question']), media_type="text/event-stream")
+    return StreamingResponse(chat(body['question']), media_type="text/event-stream")
 
 @app.get("/")
 async def homepage():
+    global memory
+    memory = [SystemMessage(content="You are a helpful assistant.")]
     return FileResponse('statics/index.html')
 
+
 if __name__ == "__main__":
-    uvicorn.run(host="0.0.0.0", port=8888, app=app)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
